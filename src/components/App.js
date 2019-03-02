5952b89e71ce80066d1da276858f40dfe3e935ee
David Ko <dkoflow@gmail.com>
2019-03-01 22:21:48 -0800

    Created PingMe React project and home page

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App(props){
+    return(
+        <div>
+            <h1>Ping Me</h1> 
+            <p>A leading app for like-minded professional to expand their network</p>
+        </div>
+    )
+}
+export default App;
